c0b03ef51a263b656e5cfed7829211ad0e09f5a7
Edward Angelo Salas <zyro670@gmail.com>
2022-05-09 20:24:41 -0400

    commit tree c76446320a2017ad2947ed5f8e8c9b0abeb2f63b

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About(props) {
+<aside>
+    <img src="props"></img>
+</aside>
+}
+
+export default About;
---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Article(props) {
+  return (
+      <article>
+          <h3>{props.title}</h3>
+          <small>{props.title}</small>          
+          <p>{props.preview}</p>
+      </article>
+  );
+}
+
+export default Article;
